0b6acb6129136181b9d545dc095d2d2fdef3d82c
milad-a-kareem <milad.kareem@gmail.com>
2023-12-18 12:19:34 +0300

    project init

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "QUI React",
+      formats: ["es", "umd"],
+      fileName: (format) => `my-react-ts-library.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
