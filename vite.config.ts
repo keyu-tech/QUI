d208117632784824ffb56875d802090a3f5f2129
milad-a-kareem <milad.kareem@gmail.com>
2023-12-19 11:59:56 +0300

    feat: button

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "QUI React",
+      name: "qui-react",
       formats: ["es", "umd"],
-      fileName: (format) => `my-react-ts-library.${format}.js`,
+      fileName: (format) => `qui-react-ts-library.${format}.js`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
